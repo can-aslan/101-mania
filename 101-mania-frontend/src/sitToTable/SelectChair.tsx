126940c5763fa9d376b103a230ccc1491cebffba
CerenAkyr <cerenakyar@gmail.com>
2024-10-20 02:54:36 +0300

    Added Avatars

--- 101-mania-frontend/src/sitToTable/SelectChair.tsx
+++ 101-mania-frontend/src/sitToTable/SelectChair.tsx
@@ -4,33 +4,37 @@ import table from "../assets/table-top.png";
 import {useState} from "react";
 function SelectChair() {
 
-    const [selectedChair, setSelectedChair] = useState(0)
+    const [selectedChair, setSelectedChair] = useState(null)
 
     return (
         <div className="main-container">
-            <div className="chair-text">
-                <h2>Hoş Geldin Kardeşim!</h2>
-                <p>Biz de oyun başlamak için seni bekliyorduk. Ama öncelikle hangi sandalyede oturmak istediğini seçebilir misin?</p>
-            </div>
-            <div className="sitting-container">
-                <div className="chair-container">
-                    <img src={chair} alt="" style={{rotate: "-90deg"}} className="chair-image" onClick={() => setSelectedChair(0)}/>
+            {selectedChair === null &&
+                <div className="chair-text">
+                    <h2>Hoş Geldin Kardeşim!</h2>
+                    <p>Biz de oyun başlamak için seni bekliyorduk. Ama öncelikle hangi sandalyede oturmak istediğini seçebilir misin?</p>
                 </div>
-                <div className="table-container">
+            }
+            {selectedChair === null &&
+                <div className="sitting-container">
                     <div className="chair-container">
-                        <img src={chair} alt="" className="chair-image" onClick={() => setSelectedChair(1)}/>
+                        <img src={chair} alt="" style={{rotate: "-90deg"}} className="chair-image" onClick={() => setSelectedChair(0)}/>
                     </div>
                     <div className="table-container">
-                        <img src={table} alt="" className="table-image"/>
+                        <div className="chair-container">
+                            <img src={chair} alt="" className="chair-image" onClick={() => setSelectedChair(1)}/>
+                        </div>
+                        <div className="table-container">
+                            <img src={table} alt="" className="table-image"/>
+                        </div>
+                        <div className="chair-container">
+                            <img src={chair} alt="" style={{rotate: "180deg"}} className="chair-image" onClick={() => setSelectedChair(2)}/>
+                        </div>
                     </div>
                     <div className="chair-container">
-                        <img src={chair} alt="" style={{rotate: "180deg"}} className="chair-image" onClick={() => setSelectedChair(2)}/>
+                        <img src={chair} alt="" style={{rotate: "90deg"}} className="chair-image" onClick={() => setSelectedChair(3)}/>
                     </div>
                 </div>
-                <div className="chair-container">
-                    <img src={chair} alt="" style={{rotate: "90deg"}} className="chair-image" onClick={() => setSelectedChair(3)}/>
-                </div>
-            </div>
+            }
         </div>
     )
 }
